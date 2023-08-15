44153006dab966ea5819d97a3b2a525aad81224a
samuel-freitas <samuelfreitassoares@outlook.com>
2023-08-15 11:28:15 -0300

    aula 3

---
+++ workspace_eclipse/RevisaoOO/src/estoque/Produto.java
@@ -0,0 +1,32 @@
+package estoque;
+
+import java.time.LocalDate;
+
+public class Produto {
+	
+	private String descricao;
+	private double valorCompra;
+	private LocalDate dataValidade;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValorCompra() {
+		return valorCompra;
+	}
+	public void setValorCompra(double valorCompra) {
+		this.valorCompra = valorCompra;
+	}
+	public LocalDate getDataValidade() {
+		return dataValidade;
+	}
+	public void setDataValidade(LocalDate dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+	
+	
+
+}
